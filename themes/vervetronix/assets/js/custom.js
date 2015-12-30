47d90ff9065526490c8103b2ebd48d834e762c69
Pratyush Pundir <pratyushpundir@icloud.com>
2015-12-30 11:45:49 -0600

    Stop linking to VueJS as it cause leftside postfix JS bug

--- themes/vervetronix/assets/js/custom.js
+++ themes/vervetronix/assets/js/custom.js
@@ -1,13 +1,3 @@
-new Vue({
-    el: '#vue-app',
-
-    methods: {
-        subscribeCustomer: function() {
-            alert('Customer Subscribed');
-        }
-    }
-});
-
 $('#subscriptionForm').on('shown.bs.modal', function () {
     $('#emailInput').focus()
 });
